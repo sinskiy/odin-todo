e2e73a77dce6a87aeca2190bb42192c04d1fb26d
sinskiy <sinskiy@proton.me>
2024-06-21 16:00:05 +0300

    feat: add actions to tasks

--- src/index.js
+++ src/index.js
@@ -2,3 +2,5 @@ import "./reset.css";
 import "./normalize.css";
 import "./helpers.css";
 import "./style.css";
+
+import Trash from "./trash.svg";
--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,10 @@ module.exports = {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.svg$/i,
+        type: "asset/resource",
+      },
     ],
   },
 };
